245280de7f07b76d4cb913e462daf5fd0c72a378
qooba <dev@qooba.net>
2022-07-29 23:26:47 +0200

    change pypi version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ DEV_REQUIRE = RAY_REQUIRE + \
 
 setup(
     name=NAME,
-    version="0.0.2",
+    version="0.0.3",
     author="Qooba",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
