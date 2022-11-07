6e63b9cd7ed631767688d778b877055a6f000421
qooba <dev@qooba.net>
2022-11-08 00:55:15 +0100

    add mlflow serve

--- yummy/cli.py
+++ yummy/cli.py
@@ -45,5 +45,46 @@ def serve_command(
     """Start rust feature server."""
     yummy_rs.serve(filename,host,port,log_level)
 
+@cli.group(name='models')
+def models_cmd():
+    """Models related commands"""
+    pass
+
+@models_cmd.command("serve")
+@click.option("--host","-h",
+              type=click.STRING,
+              default="127.0.0.1",
+              show_default=True,
+              help='Host for the feature server.'
+              )
+@click.option("--port","-p",
+              type=click.INT,
+              default=6566,
+              show_default=True,
+              help='Port for the feature server.'
+              )
+@click.option("--model-uri","-m",
+              type=click.STRING,
+              required=True,
+              help='Local model path.'
+              )
+@click.option("--log-level",
+              type=click.STRING,
+              default="error",
+              show_default=True,
+              help='Log level for the feature server. One of: debug, info, error, warn'
+              )
+@click.pass_context
+def serve_command(
+    ctx: click.Context,
+    host: str,
+    port: int,
+    filename: str,
+    log_level: str,
+):
+    """Start rust feature server."""
+    yummy_rs.serve(filename,host,port,log_level)
+
+
 if __name__ == '__main__':
     cli()
