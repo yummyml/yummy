3b18e5a1a97a6aaac57b864a4df726b5d208f30e
qooba <dev@qooba.net>
2023-04-01 13:50:36 +0200

    add py packages

--- yummy-rs/yummy/src/main.rs
+++ yummy-rs/yummy/src/main.rs
@@ -1,5 +1,4 @@
 use clap::{arg, Command};
-use yummy_delta::apply_delta;
 
 fn cli() -> Command {
     Command::new("yummy")
@@ -30,7 +29,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let file = sub_sub_matches
                     .get_one::<String>("filename")
                     .expect("required");
-                yummy_delta::apply_delta(file);
+                apply_delta(file).await?
             }
             _ => unreachable!(),
         },
@@ -38,4 +37,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+#[cfg(feature = "yummy-delta")]
+async fn apply_delta(file: &String) -> std::io::Result<()> {
+    yummy_delta::apply_delta(file.clone()).await?
+}
+
+#[cfg(not(feature = "yummy-delta"))]
+async fn apply_delta(_file: &String) -> std::io::Result<()> {
+    unreachable!()
 }
